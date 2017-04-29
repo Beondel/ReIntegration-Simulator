37329e06bc227fc16842b71a99f78b6b91b3db90
Beondel <bengmacmillan@gmail.com>
2017-04-29 15:20:28 -0700

    core mechanics done

--- Assets/Runner.cs
+++ Assets/Runner.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour {
 
-	private float speed;
 	public Rigidbody2D _rigidbody;
 	public Transform _transform;
 	public BoxCollider2D _collider;
+	public Sprite[] textures;
 
 	void Start () {
-		speed = 10;
-		isOnGround = false;
 		_transform = GetComponent(typeof(Transform)) as Transform;
 		_rigidbody = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
-		_collider = GetComponent (typeof(BoxCollider2D)) as BoxCollider2D;
+		_collider = GetComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+		_rigidbody.freezeRotation = true;
 	}
 		
 	void Update () {
@@ -22,27 +21,23 @@ public class Runner : MonoBehaviour {
 	}
 
 	void movement(){
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Translate (Vector2.right * speed * Time.deltaTime);
+		if (isOnGround(GameObject.Find("ground_placeholder").GetComponent<Collider2D>())){
+			if (Time.frameCount % 2 == 0) {
+				runAnimation (Time.frameCount / 2);
+			}
 		} 
-		if (Input.GetKeyDown (KeyCode.UpArrow) && isOnGround(GameObject.Find("ground_placeholder").collider2D)) {
-			_rigidbody.AddForce (new Vector2(0, 7f), ForceMode2D.Impulse);
-		}
-	}
-	/*
-	void onCollisionEnter2D(Collider2D other){
-		if (other.gameObject.tag == "ground") {
-			isOnGround = true;
+		if (Input.GetKeyDown (KeyCode.UpArrow) && isOnGround(GameObject.Find("ground_placeholder").GetComponent<Collider2D>())) {
+			_rigidbody.AddForce (new Vector2(0, 10f), ForceMode2D.Impulse);
+
+			this.gameObject.GetComponent<SpriteRenderer> ().sprite = textures [4];
 		}
 	}
 
-	void onCollisionExit2D(Collider2D other){
-		if (other.gameObject.tag == "ground") {
-			isOnGround = false;
-		}
-	}
-*/
 	bool isOnGround(Collider2D other){
 		return _collider.IsTouching (other);
+	}
+
+	void runAnimation(int time){
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = textures [time % 8];
 	}
 }
---
+++ Assets/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] obstacles;
+	public float spawnWait;
+	public float spawnMostWait;
+	public float spawnLeastWait;
+	public int startWait;
+
+	int randEnemy;
+
+	void Start () {
+		StartCoroutine (Spawn());
+	}
+
+
+	void Update () {
+		spawnWait = Random.Range (spawnLeastWait, spawnMostWait);
+	}
+
+	IEnumerator Spawn(){
+		yield return new WaitForSeconds (startWait);
+
+		while (true) {
+			randEnemy = Random.Range (0, obstacles.Length);
+			Vector3 spawnPosition = GameObject.Find ("spawner").transform.position;
+			Instantiate (obstacles[randEnemy], spawnPosition, gameObject.transform.rotation);
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
